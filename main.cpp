24a19d86236f118be3bb7cd983c69de3d68318ed
fethicekinmez <fethicekinmez@gmail.com>
2025-04-12 23:04:10 +0300

    Add project template files both headers and modules

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "utilities.h"
+#include <iostream>
+
+int main() {
+    int a = 29;
+    int b = 99;
+    std::cout << "Hello World, This is a test" << std::endl;
+    int c = a+b;
+    std::cout << c << std::endl;    
+}
